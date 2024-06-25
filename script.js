9925d8a6427b86215e9aa5f4601993322b009809
CHIRANJEET DWIVEDY <154692196+CHIRANJEET12@users.noreply.github.com>
2024-06-25 17:59:41 +0530

    commit
    footer

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ const observer = new IntersectionObserver((entries, observer) => {
         }
     });
 }, options);
+<<<<<<< Updated upstream
 // observer.observe(nor);
 observer.observe(mid);
 observer.observe(cards);
@@ -82,6 +83,27 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
+=======
+observer.observe(nor);
+observer.observe(mid);
+observer.observe(vid);
+observer.observe(select);
+observer.observe(introcard);
+observer.observe(down);
+
+
+observer.observe(cards);
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    centeredSlides: true,
+    spaceBetween: 30,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+>>>>>>> Stashed changes
 
   function menuAnimation() {
 
@@ -128,6 +150,7 @@ let tween = gsap.to(".mar_in", {
     } else {
         document.body.style.backgroundColor = 'linear-gradient(141deg, #ffffff 0%, #f8ffd1 51%, #ffed9d 75%)'; // Default background color
     }
+<<<<<<< Updated upstream
 });
 
 
@@ -153,3 +176,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     checkVisibility();
 });
+=======
+});
+>>>>>>> Stashed changes
